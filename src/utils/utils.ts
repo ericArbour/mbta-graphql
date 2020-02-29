95378eefd2bf28dc0e7b7c438674dee91617a476
Eric Arbour <arboureric@gmail.com>
2020-02-29 13:36:55 -0500

    Add fragment support to field getters.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,17 +1,42 @@
-import { GraphQLResolveInfo } from "graphql";
+import {
+  GraphQLResolveInfo,
+  SelectionNode,
+  FragmentDefinitionNode
+} from "graphql";
 import { ApolloError } from "apollo-server";
 
 export function getFieldsFromInfo(info: GraphQLResolveInfo): string[] {
   const selections = info.fieldNodes[0].selectionSet?.selections || [];
 
-  return (
-    selections
-      .map(fieldNode =>
-        fieldNode.kind === "Field" ? fieldNode.name.value : ""
-      )
-      // id field is always returned from MBTA api and never needs to be specified in fields
-      .filter(field => field && field !== "id")
-  );
+  return recursiveFieldsGetter(selections, info.fragments);
+}
+
+function recursiveFieldsGetter(
+  selections: readonly SelectionNode[],
+  maybeFragmentMap?: FragmentMap
+): string[] {
+  const fields = selections.flatMap(fieldNode => {
+    if (fieldNode.kind === "Field") return fieldNode.name.value;
+    if (fieldNode.kind === "FragmentSpread" && isFragmentMap(maybeFragmentMap))
+      return recursiveFieldsGetter(
+        maybeFragmentMap[fieldNode.name.value].selectionSet.selections,
+        maybeFragmentMap
+      );
+    return "";
+  });
+
+  // id field is always returned from MBTA api and never needs to be specified in fields
+  return fields.filter(field => field && field !== "id");
+}
+
+type FragmentMap = {
+  [key: string]: FragmentDefinitionNode;
+};
+
+function isFragmentMap(
+  maybeFragmentMap: FragmentMap | undefined
+): maybeFragmentMap is FragmentMap {
+  return maybeFragmentMap !== undefined;
 }
 
 export class MbtaRESTError extends ApolloError {
